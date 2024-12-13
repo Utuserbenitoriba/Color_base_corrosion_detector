db3c7b4de46debba1b89412c182833ef4aff3437
Utuserbenitoriba <benitoalexrp@utexas.edu>
2024-12-13 00:59:50 -0600

    Mask overlay, regtangle contour, date on the picture ignored

--- color_detector.py
+++ color_detector.py
@@ -2,15 +2,20 @@ import numpy as np
 import argparse
 import cv2
 from pathlib import Path
-a = 14 
-# H S V color ranges for corrosion
+
+# H S V color ranges for corrosion. (Hue, Saturation, Value)
 lower_corrosion = np.array([0,50,50])
-upper_corrosion = np.array([11,255,255])
-
-lower_corrosion2 = np.array([175,50,50])
+upper_corrosion = np.array([12,255,255])
+# Corrosion color red is split into two ranges in the hsv color space
+lower_corrosion2 = np.array([165,50,50])
 upper_corrosion2 = np.array([179,255,255])
+# Defining a more rigurous range for the corrosion inside the rectangle
+lower_corrosion_inside = np.array([0,50,50])
+upper_corrosion_inside = np.array([17,255,255])
 
-image_path = Path('corrosion_pictures/1.png')
+
+# Load the image
+image_path = Path('corrosion_pictures/3.png')
 image = cv2.imread(image_path)
 
 # Check if the image was loaded successfully
@@ -20,19 +25,43 @@ else:
     print("Image loaded successfully!")
     print("Image shape:", image.shape)  # Show dimensions (height, width, channels)
 
+# Convert the image to the HSV color space
 image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(image_hsv, lower_corrosion, upper_corrosion)
 
+# Create a mask for the corrosion color
+mask1 = cv2.inRange(image_hsv, lower_corrosion, upper_corrosion)
+mask2 = cv2.inRange(image_hsv, lower_corrosion2, upper_corrosion2)
+mask = cv2.bitwise_or(mask1, mask2)
+# Create a mask for the inside of the rectangle
+mask_inside = cv2.inRange(image_hsv, lower_corrosion_inside, upper_corrosion_inside)
+
+# Ignoring a rectangle in the image (date of in the picture)
+x1, y1 = 600, 510
+x3, y3 = 800, 550
+mask[y1:y3, x1:x3] = 0
+
+# Find contours in the mask
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+mask_colored = np.zeros_like(image)  # Initialize a colored mask with zeros
+
 if len(contours) != 0:
     for contour in contours:
-        if cv2.contourArea(contour) > 100:
+        if cv2.contourArea(contour) > 30:
             x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+            # Change the color of the mask inside the rectangle to green
+            mask_colored[y:y+h, x:x+w][mask_inside[y:y+h, x:x+w] == 255] = [0, 255, 0]
 
-cv2.imshow('Mask', mask)
-cv2.imshow('hsv Image', image_hsv)
+# Overlay the original image and the colored mask with a transparency of 50%
+overlay = cv2.addWeighted(image, 1, mask_colored, 0.5, 0)
+
+#cv2.imshow('Mask1', mask1)
+#cv2.imshow('Mask2', mask2)
 cv2.imshow('Original Image', image)
+# cv2.imshow('Mask_inside', mask_inside)
+cv2.imshow('Mask', mask)
+# cv2.imshow('hsv Image', image_hsv)
+cv2.imshow('Overlay Image', overlay)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
