1ac736fdadb7895616ffd5888e06dd2bfa32bb4c
Utuserbenitoriba <benitoalexrp@utexas.edu>
2024-12-13 05:09:01 -0600

    Area with one picture

--- color_detector.py
+++ color_detector.py
@@ -3,6 +3,7 @@ import argparse
 import cv2
 from pathlib import Path
 import os
+import sys
 
 # H S V color ranges for corrosion. (Hue, Saturation, Value)
 lower_corrosion = np.array([0,50,50])
@@ -30,89 +31,92 @@ upper_dark_brown = np.array([20, 255, 100])
 
 
 # Load the image
-# image_path = Path('corrosion_pictures/3.png')
-# image = cv2.imread(image_path)
-input_dir = Path('corrosion_pictures')
+image_path = Path('corrosion_pictures/13.png')
+image = cv2.imread(image_path)
+hight, wide, m = image.shape
+
 output_dir = Path('processed_pictures')
 output_dir.mkdir(exist_ok=True)
 
-for image_path in input_dir.glob('*.png'):
-    image = cv2.imread(str(image_path))
+properties = float (sys.argv[1]) # gal/m2 
 
-    # Check if the image was loaded successfully
-    if image is None:
-        print("Error: Could not load image {image_path}.")
-        continue
-    else:
-        print(f"Processing image {image_path}...")
-        #print("Image shape:", image.shape)  # Show dimensions (height, width, channels)
-
-    # Convert the image to the HSV color space
-    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Create a mask for the corrosion color
-    mask1 = cv2.inRange(image_hsv, lower_corrosion, upper_corrosion)
-    mask2 = cv2.inRange(image_hsv, lower_corrosion2, upper_corrosion2)
-
-    # Combine the two masks to look for corrosion
-    mask = cv2.bitwise_or(mask1, mask2)
-
-    # Create a mask for the inside of the rectangle, different colors
-    mask_broader = cv2.inRange(image_hsv, lower_corrosion_inside, upper_corrosion_inside)
-    mask_inside = cv2.inRange(image_hsv, lower_corrosion_inside, upper_corrosion_inside)
-    mask_black = cv2.inRange(image_hsv, lower_black, upper_black)
-    mask_dark_red = cv2.inRange(image_hsv, lower_dark_red, upper_dark_red)
-    mask_brown = cv2.inRange(image_hsv, lower_brown, upper_brown)
-    mask_dark_brown = cv2.inRange(image_hsv, lower_dark_brown, upper_dark_brown)
-
-    # Combine all masks
-    mask_inside = cv2.bitwise_or(mask_broader, mask_black)
-    mask_inside = cv2.bitwise_or(mask_inside, mask_dark_red)
-    mask_inside = cv2.bitwise_or(mask_inside, mask_brown)
-    mask_inside = cv2.bitwise_or(mask_inside, mask_dark_brown)
+reference = float (sys.argv[2]) # meters
 
 
-    # Ignoring a rectangle in the image (date of in the picture)
-    x1, y1 = 600, 510
-    x3, y3 = 800, 550
-    mask[y1:y3, x1:x3] = 0
+# Check if the image was loaded successfully
+if image is None:
+    print("Error: Could not load image {image_path}.")
+    exit()
+else:
+    print(f"Image loaded successfully")
 
-    # Find contours in the mask
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask_colored = np.zeros_like(image)  # Initialize a colored mask with zeros
+# Convert the image to the HSV color space
+image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    if len(contours) != 0:
-        for contour in contours:
-            if cv2.contourArea(contour) > 30:
-                x, y, w, h = cv2.boundingRect(contour)
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Create a mask for the corrosion color
+mask1 = cv2.inRange(image_hsv, lower_corrosion, upper_corrosion)
+mask2 = cv2.inRange(image_hsv, lower_corrosion2, upper_corrosion2)
 
-                # Change the color of the mask inside the rectangle to green
-                mask_colored[y:y+h, x:x+w][mask_inside[y:y+h, x:x+w] == 255] = [0, 255, 0]
+# Combine the two masks to look for corrosion
+mask = cv2.bitwise_or(mask1, mask2)
 
-                # Label the rectangle
-                text = "Rust Zone"
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 0.5
-                font_color = (0, 255, 0)
-                thickness = 1
-                text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
-                text_x = x
-                text_y = y + h + text_size[1] + 5  # 5 pixels below the rectangle
-                cv2.putText(image, text, (text_x, text_y), font, font_scale, font_color, thickness)
+# Create a mask for the inside of the rectangle, different colors
+mask_broader = cv2.inRange(image_hsv, lower_corrosion_inside, upper_corrosion_inside)
+mask_inside = cv2.inRange(image_hsv, lower_corrosion_inside, upper_corrosion_inside)
+mask_black = cv2.inRange(image_hsv, lower_black, upper_black)
+mask_dark_red = cv2.inRange(image_hsv, lower_dark_red, upper_dark_red)
+mask_brown = cv2.inRange(image_hsv, lower_brown, upper_brown)
+mask_dark_brown = cv2.inRange(image_hsv, lower_dark_brown, upper_dark_brown)
+
+# Combine all masks
+mask_inside = cv2.bitwise_or(mask_broader, mask_black)
+mask_inside = cv2.bitwise_or(mask_inside, mask_dark_red)
+mask_inside = cv2.bitwise_or(mask_inside, mask_brown)
+mask_inside = cv2.bitwise_or(mask_inside, mask_dark_brown)
 
 
-    # Save the result image in the output directory
-    output_path = output_dir / image_path.name
-    cv2.imwrite(str(output_path), image)
+# Ignoring a rectangle in the image (date of in the picture)
+x1, y1 = 600, 510
+x3, y3 = 800, 550
+mask[y1:y3, x1:x3] = 0
 
-    # cv2.imshow('Mask1', mask1)
-    # cv2.imshow('Mask2', mask2)
-    # cv2.imshow('Mask_black', mask_black)
-    # cv2.imshow('Original Image', image)
-    # cv2.imshow('Mask_inside', mask_inside)
-    # cv2.imshow('Mask', mask)
-    # cv2.imshow('hsv Image', image_hsv)
-    # cv2.imshow('Overlay Image', overlay)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+# Find contours in the mask
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+mask_colored = np.zeros_like(image)  # Initialize a colored mask with zeros
+
+if len(contours) != 0:
+    for contour in contours:
+        if cv2.contourArea(contour) > 30:
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Change the color of the mask inside the rectangle to green
+            mask_colored[y:y+h, x:x+w][mask_inside[y:y+h, x:x+w] == 255] = [0, 255, 0]
+
+            # Label the rectangle
+            text = "Rust Zone"
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.5
+            font_color = (0, 255, 0)
+            thickness = 1
+            text_size = cv2.getTextSize(text, font, font_scale, thickness)[0]
+            text_x = x
+            text_y = y + h + text_size[1] + 5  # 5 pixels below the rectangle
+            cv2.putText(image, text, (text_x, text_y), font, font_scale, font_color, thickness)
+
+
+
+cv2.imshow("Corrosion detection", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# Ensure the mask is single-channel
+if len(mask_colored.shape) == 3:
+    mask_colored = cv2.cvtColor(mask_colored, cv2.COLOR_BGR2GRAY)
+# Calculate the area of the mask
+mask_area = cv2.countNonZero(mask_colored)
+print(f"If the hight of the image represent {reference} meters,\nand the properties of the paint specify {properties} gal/m2,\nYou will need: {round (mask_area*(reference/hight)**2*properties, 2)} gals of paint")
+
+# Save the result image in the output directory
+output_path = output_dir / image_path.name
+cv2.imwrite(str(output_path), image)
+
